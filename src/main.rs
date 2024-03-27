1356662076713357d81120f5437dfa7b3a30fd55
SmallPlayz <900raiyan@gmail.com>
2024-03-27 15:20:38 -0400

    created button methods.

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,29 @@ fn main() -> Result<(), slint::PlatformError> {
 
     let ui_handle = ui.as_weak();
 
+    ui.on_open_note({ 
+        let ui_handle = ui_handle.clone();
+        move || {
+            let ui = ui_handle.unwrap();
+            println!("User has clicked: open");
+        }
+    });
+
+    ui.on_save_note({
+        let ui_handle = ui_handle.clone();
+        move || {
+            let ui = ui_handle.unwrap();
+            println!("User has clicked: save");
+        }
+    });
+
+    ui.on_new_note({ 
+        let ui_handle = ui_handle.clone();
+        move || {
+            let ui = ui_handle.unwrap();
+            println!("User has clicked: new");
+        }
+    });
+
     ui.run()
 }
